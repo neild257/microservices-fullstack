c80e85ff166ccc6982dcb2654f22cbb967d9db6f
Harshit Pareek <harshitpareek@Harshits-MacBook-Pro.local>
2020-05-16 20:02:14 -0700

    feat(microservice not working moderation service)

--- comments/app.js
+++ comments/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4001;
 
 const commentsByPostId = {};
 
@@ -32,10 +32,13 @@ app.post('/posts/:id/comments', async (req, res) => {
     const currentComments = commentsByPostId[req.params.id] || [];
     currentComments.push({
         id: commentId,
-        content
+        content,
+        moderationStatus: 'pending'
     });
 
     commentsByPostId[req.params.id] = currentComments;
+
+    console.log('Inside the CommentCreated Event in Comment Service and payload is:', commentsByPostId[req.params.id]);
 
     // sending the events to the events bus
     await axios.post('http://localhost:4002/events', {
@@ -43,15 +46,33 @@ app.post('/posts/:id/comments', async (req, res) => {
         payload: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            moderationStatus: 'pending'
         },
     });
 
     res.send(commentsByPostId[req.params.id]);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log(`Received Event in Comments Service: ${req.body.type}`);
+    const { type, payload } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { moderationStatus, postId, id, content } = payload;
+        console.log('Inside Comment Moderated Event of Comments Service with Payload', payload);
+        commentsByPostId[postId].find((comment) => comment.id === id).moderationStatus = moderationStatus;
+
+        await axios.post('http://localhost:4002/events', {
+            type: 'CommentUpdated',
+            payload: {
+                id,
+                moderationStatus,
+                content,
+                postId
+            }
+        });
+    }
 
     res.send({});
 });
